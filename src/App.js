07959a315540f781c7078818e0eeb2756b0744f3
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-03 03:51:20 +0100

    Refactors to use named exports and index.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import axios from 'axios';
-import Home from './potter/components/Home';
-import SortingHat from './potter/components/SortingHat';
-import Spells from './potter/components/Spells';
-import Characters from './potter/components/Characters';
+import { Home, SortingHat, Spells, Characters } from './components';
+
 
 
 import './App.css';
