da256afd268357bdb58dc3400bad96e914688706
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-03 03:27:31 +0100

    Adds Home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import axios from 'axios';
+import Home from './potter/components/Home';
 
 
 import './App.css';
@@ -8,6 +9,11 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
+      <Router>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Router>
     </div>
   );
 }
